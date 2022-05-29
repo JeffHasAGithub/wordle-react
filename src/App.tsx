3b3bfdc120d7a16109476e26e20ccead2fbebaa0
Jeff <jeffmooreweb@gmail.com>
2022-05-28 20:19:56 -0500

    Added Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css'
 
+import Header from "./components/header"
+
 
 function App() {
 	return <>
+		<Header
+			text="Wordle"
+			textColor="lemonchiffon"
+			shadowColors={["lightcoral", "lightskyblue", "lightgreen"]}
+		/>
 	</>
 }
 
---
+++ src/components/header/index.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import styles from "./styles.module.css"
+
+type Props = {
+	text: string;
+	textColor: string;
+	shadowColors: Array<string>
+}
+
+export default function Header({ text, shadowColors, textColor }: Props) {
+	const [color, setColor] = React.useState(0);
+	const [position, setPosition] = React.useState(0);
+	
+	const word = React.useMemo(() => text.split(""), [text]);
+	const shadows = React.useMemo(() => word.map((_) => "darkgray"), [textColor]);
+	
+	React.useEffect(() => {
+			shadows[position] = shadowColors[color];
+			const timerId = setTimeout(() => {
+				if (position === word.length)
+					return;
+
+				if (color === shadowColors.length - 1)
+					setColor(0);
+				else
+					setColor(color + 1);
+				
+				setPosition(position + 1);
+			}, 200);
+
+			return () => clearTimeout(timerId);
+	}, [position])
+
+	return (
+		<>
+			<h1 className={styles.container}>
+			{
+				word.map((letter, i) => {
+					return <span key={i} className={styles.text} style={{color: `${textColor}`, textShadow: `5px 5px ${shadows[i]}`}}>{letter}</span>
+				})
+			}
+			</h1>
+		</>
+	)
+}
